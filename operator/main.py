d8414bc4efe3e26123a799a55a36c36e914fbd35
himawanTIF <himawan1700018032@webmail.uad.ac.id>
2025-07-11 00:09:27 +0700

    Happy day #4 Operator

---
+++ operator/main.py
@@ -0,0 +1,53 @@
+# Operator aritmatika
+# + -> operasi tambah
+num = 2 + 2
+print(num)
+
+# unary + -> penanda nilai positif
+num = +2
+print(num)
+
+# - -> operasi pengurangan
+num = 3 - 2
+print(num)
+
+# unary - -> penanda nilai negatif
+num = -2
+print(num)
+
+# * -> operasi perkalian
+num = 3 * 3
+print(num)
+
+# / -> operasi pembagian
+num = 8 / 2
+print(num)
+
+# // -> operasi bagi dengan hasil dibulatkan ke bawah
+num = 10 // 3
+print(num)
+
+# & -> operasi modulo (pencarian sisa hasil bagi)
+num = 7 % 2
+print(num)
+
+# ** -> operasi pangkat
+num = 3 ** 2
+print(num)
+
+# Operator assignment (=) digunakan untuk penentuan nilai sekaligus deklarasi variabel
+# deklarasi variabel
+num_1 = 12
+num_2 = 1
+# deklarasi variabel dengan isi nilai hasil oeprasi aritmatika num_1 + num_2
+num_3 = num_1 + num_2
+print(num_3)
+
+# Operator perbandingan dengan tipe data boolean (bool) yang menghasilkan nilai kebenaran
+# Dua nilai kebenaran yang ada yaitu True (benar) atau False (salah)
+# operator == (apakah kiri sama dengan kanan)
+res = 4 == 5
+print(res)
+# operator != (apakah kiri tidak sama dengan kanan)
+res = 4 != 5
+print(res)
