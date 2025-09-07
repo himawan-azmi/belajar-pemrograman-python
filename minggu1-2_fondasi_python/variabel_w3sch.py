306ce88e14bde1ea3ad08be6b21198d446609233
himawan-azmi <himawan1700018032@webmail.uad.ac.id>
2025-09-07 22:23:08 +0700

    Update Minggu1-2

---
+++ minggu1-2_fondasi_python/variabel_w3sch.py
@@ -0,0 +1,35 @@
+print('Casting')
+# Casting digunakan untuk menentukan tipe data variabel secara spesifik
+x = str(3)  # x akan bernilai '3'
+y = int(3)  # y akan bernilai 3
+z = float(3) # z akan bernilai 3.0
+print(x)
+print(y)
+print(z)
+#
+print("==============================")
+#
+print('Mendapatkan tipe data dengan type()')
+# Gunakan fungsi type() untuk mendapatkan tipe data variabel
+x = 5 
+y = "John"
+print(type(x))
+print(type(y))
+#
+print('-------------------------------')
+print('Nama variabel itu case-sensitive')
+# Nama variabel pada python adalah case-sensitive
+a = 4
+A = 'Sally'
+# A tidak akan menimpa a
+print(a)
+print(A)
+#
+print('----------------------------')
+# Nama variabel tidak boleh diawali dengan angka atau nomor
+print('Multi Words Variable Name')
+# Nama variabel dengan lebih dari satu kata bisa sulit untuk dibaca
+# Ada beberapa teknik yang dapat digunakan untuk memudahkan keterbacaannya
+print('Teknik Penulisan Nama Variabel Multi Kata')
+print('1. Camel Case')
+#
